e1e261fa4d62b9a4d98f76e5187c02a3a0766e3f
ugiewon <urszula.giewon@gmail.com>
2020-08-06 12:07:21 +0200

    Updated developer tools and tips page

--- 1-GV_developer_tool_and_tips/js/js.js
+++ 1-GV_developer_tool_and_tips/js/js.js
@@ -122,4 +122,139 @@ $(function () {
   setToActiveCardNumber($('[cart-number="2"]'));
   setToActiveCardNumber($('[cart-number="3"]'));
   setToActiveCardNumber($('[cart-number="4"]'));
+
+  function checkReservation(button) {
+    button.on("click", function () {
+      switch ($(this).attr("number")) {
+        case "1":
+          $(this).attr("value", 1);
+          break;
+        case "2":
+          $(this).attr("value", 2);
+          break;
+        case "3":
+          $(this).attr("value", 3);
+          break;
+      }
+    });
+  }
+  checkReservation($('[href="#book-a-seat"]:nth-of-type(1)'));
+  checkReservation($('[href="#book-a-seat"]:nth-of-type(2)'));
+  checkReservation($('[href="#book-a-seat"]:nth-of-type(3)'));
+
+  function countSeats() {
+    let availableSeats = parseInt($(".seats").attr("value"));
+    let firstPackage = parseInt($("#buy1").attr("value"));
+    let secondPackage = parseInt($("#buy2").attr("value"));
+    let thirdPackage = parseInt($("#buy3").attr("value"));
+
+    availableSeats =
+      availableSeats - (firstPackage + secondPackage + thirdPackage);
+
+    return availableSeats;
+  }
+
+  function countAvailableSeats() {
+    if (countSeats() == 0 || countSeats() < 0) {
+      $(".seats").html(" no available ");
+    } else {
+      $(".seats").html(countSeats());
+      $(".seats").attr("value", countSeats());
+    }
+  }
+
+  function checkIfPopulated(item) {
+    if (item.val().length == 0) {
+      item.addClass("error");
+      item.attr("placeholder", "Please fill the field");
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function addError(item) {
+    item.addClass("error");
+  }
+
+  function removeError(item) {
+    item.removeClass("error");
+  }
+
+  function validateName(inputId) {
+    if (inputId.val().length <= 3) {
+      addError(inputId);
+      $("span.error.name").show().text("Name too short");
+      return false;
+    } else if (inputId.val().length >= 30) {
+      addError(inputId);
+      $("span.error.name").show().text("Name too long");
+      return false;
+    } else {
+      $("span.error.name").hide();
+      removeError(inputId);
+      return true;
+    }
+  }
+
+  function validateMail(inputId) {
+    let pattern = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+    if (!pattern.test(inputId.val())) {
+      addError(inputId);
+      $("span.error.mail").show().text("Email format is invalid");
+      return false;
+    } else {
+      removeError(inputId);
+      $("span.error.mail").hide();
+      return true;
+    }
+  }
+
+  function validatePhone(inputId) {
+    if (isNaN(inputId.val())) {
+      addError(inputId);
+      $("span.error.phone").show().text("Only numbers allowed");
+      return false;
+    } else if (inputId.val().length != 9) {
+      addError(inputId);
+      $("span.error.phone").show().text("Number lenght invalid");
+      return false;
+    } else {
+      removeError(inputId);
+      $("span.error.phone").hide();
+      return true;
+    }
+  }
+
+  $("#name").on("change", function () {
+    validateName($(this));
+  });
+
+  $("#mail").on("change", function () {
+    validateMail($(this));
+  });
+
+  $("#phone").on("change", function () {
+    validatePhone($(this));
+  });
+
+  function validateForm() {
+    $("form").submit(function () {
+      checkIfPopulated($("#name"));
+      checkIfPopulated($("#phone"));
+      checkIfPopulated($("#mail"));
+      if (
+        !checkIfPopulated($("#name")) ||
+        !checkIfPopulated($("#phone")) ||
+        !checkIfPopulated($("#mail"))
+      ) {
+        return false;
+      } else {
+        countAvailableSeats();
+        return true;
+      }
+    });
+  }
+
+  validateForm();
 });
